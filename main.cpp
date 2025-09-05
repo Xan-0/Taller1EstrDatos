bbeb13c31ee4c7596fbce8a195e3b6a8a1ca1f49
Xan-0 <johanalexis650@gmail.com>
2025-09-05 16:19:26 -0400

    Update main.cpp
    comprobación de funciones básicas

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
-#include "LinkedList.hpp"
-
-/*test*/
+#include "Curso.h"
 
 int main()
-{
-    LinkedList *lista = new LinkedList();
-    Node *x = new Node();
-    x -> setValue(5);
-    lista -> add(x -> getValue(), 0);
-    std::cout<<x->getValue()<<std::endl;
-    std::cout<<lista->get(0);
-
+{   
+    Alumno *al = new Alumno(57,"Juan","Alzamora","ITI","2025");
+    std::cout<<"id: ";
+    std::cout<<al->getId()<< std::endl;
+    std::cout<<"estudiante: ";
+    std::cout<<al->getNombre() + " ";
+    
+    Curso *prog = new Curso(21, "Programacion",5,"ITI","David Beckios");
+    prog -> add(al);
+    
+    std::cout<<prog->get(57)->getApellido()<< std::endl;
+    std::cout<<"curso: ";
+    std::cout<<prog->getNombre()<< std::endl;
+    
+    al -> add(5, "Programacion");
+    std::cout<<"nota 1: ";
+    std::cout<<al->getNotas()->getNota()<< std::endl;
+    
+    std::cout<<"eliminando curso..."<< std::endl;
+    prog->~Curso();
+    std::cout<<"comprobacion de estado de la id: ";
+    std::cout<<al->getId();
     return 0;
 }
