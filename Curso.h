3f93651f75245fc9d08dca5be1274cc8250b5fd1
Xan-0 <johanalexis650@gmail.com>
2025-09-11 19:41:55 -0300

    Update 3 Curso.h

--- Curso.h
+++ Curso.h
@@ -4,39 +4,33 @@
 class Curso {
     private:
         int id;
-    	std::string nombre;
+    	string nombre;
     	int capacidad;
-    	std::string carrera;
-    	std::string profesor;
+    	string carrera;
+    	string profesor;
     	int cant_alumnos;
-    	Alumno *alumnos_start;
     	Curso *next;
     public:
-        Curso(int id, std::string, int capacidad, std::string carrera,
-        std::string profesor);
+        Curso(int id, string, int capacidad, string carrera,
+        string profesor);
 	
         void setId(int id);
-    	void setNombre(std::string nombre);
+    	void setNombre(string nombre);
     	void setCapacidad(int capacidad);
-    	void setCarrera(std::string carrera);
-    	void setProfesor(std::string profesor);
+    	void setCarrera(string carrera);
+    	void setProfesor(string profesor);
+    	void setCantAlumnos(int cant_alumnos);
         void setNext(Curso *next);
 	
-    	void add(Alumno *alumno);
-    	void remove(int id);
-    	Alumno *get(int id);
     	void print();
-    	void print_alumno(std::string nombre_alumno);
-    	void print_alumnos();
-    	double promedio(int id_alumno);
-        bool isEmpty();
 	
         int getId();
-    	std::string getNombre();
+    	string getNombre();
     	int getCapacidad();
-    	std::string getCarrera();
-    	std::string getProfesor();
-    	Alumno *getAlumnos();
+    	string getCarrera();
+    	string getProfesor();
+    	int getCantAlumnos();
         Curso *getNext();
+        
         ~Curso();
 };
